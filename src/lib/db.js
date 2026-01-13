245ef1e66b8e269e97e9371eb218d524216b9e86
Karna <kn8610519@gmail.com>
2026-01-13 14:54:17 +0530

    connection corrected

--- src/lib/db.js
+++ src/lib/db.js
@@ -5,11 +5,11 @@ let connection;
 export async function getDB() {
   if (!connection) {
     connection = await mysql.createPool({
-      host: "shinkansen.proxy.rlwy.net",
-      port:"18717",
-      user: "root",
-      password: "OuRnwjCzUwfIVzpZrtBWYMOUvFhjgbYO",
-      database: "railway",
+      host: process.env.MYSQL_HOST,
+      port: process.env.MYSQL_PORT,
+      user: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
     });
   }
   return connection;
